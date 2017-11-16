853e9d7db4698ccf8e5622061735f04913ce5fca
programmiertier <zimmermannveronique@protonmail.com>
2017-11-16 10:01:54 +0100

    Syntax
    Syntax in xaml

---
+++ Syntax_WpfApplication/Syntax_WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Syntax_WpfApplication
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            knopf1.Content = "kno1";
+            knopf1.Margin = new Thickness(-399-50, -299-200, 0, 0);
+            // knopf2.Content = "kno2";
+            // knopf3.Content = "kno3 weil er so toll is";
+            
+        }
+    }
+}
