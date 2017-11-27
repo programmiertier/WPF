61bc9b03f91f8a8f3f7b7d265520499f67f1f144
programmiertier <zimmermannveronique@protonmail.com>
2017-11-27 08:54:26 +0100

    Button Animation
    Animationen

---
+++ ButtonAnimation_WpfApplication/ButtonAnimation_WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonAnimation_WpfApplication
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void Winodw_Loaded(object sender, RoutedEventArgs e) // sobald der Schirm einmal komplett geladen ist
+        {
+            MessageBox.Show(sender.ToString() + e.ToString());
+        }
+    }
+}
