787b1ccd00689d69f1a1557dfc0442dc42cd681d
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2017-11-30 13:57:20 +0100

    Canvas Thickness Duration

--- EigeneKlasseIn_WpfApplication/EigeneKlasseIn_WpfApplication/MainWindow.xaml.cs
+++ EigeneKlasseIn_WpfApplication/EigeneKlasseIn_WpfApplication/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +21,7 @@ namespace EigeneKlasseIn_WpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int anzKunden;
         public MainWindow()
         {
             InitializeComponent();
@@ -33,18 +35,52 @@ namespace EigeneKlasseIn_WpfApplication
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Kunde heinz = new Kunde();
+            /* Kunde heinz = new Kunde();
             raumdarstellung.Children.Add(new Image() { Source = heinz.wagen.Source });
-            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count-1], 30);
+            // Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count-1], 30);
             Kunde meier = new Kunde();
             raumdarstellung.Children.Add(new Image() { Source = meier.wagen.Source });
-            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 60);
+            // Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 60);
             Kunde schulz = new Kunde();
             raumdarstellung.Children.Add(new Image() { Source = schulz.wagen.Source });
-            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 90);
+            // Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 90);
             Kunde schmidt = new Kunde();
             raumdarstellung.Children.Add(new Image() { Source = schmidt.wagen.Source });
-            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 120);
+            // Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 120); */
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            anzKunden = Int32.Parse(slider.Value.ToString());
+            bool autoreverse = true;
+            
+
+            ThicknessAnimation x = new ThicknessAnimation();
+            Thickness bla = new Thickness();
+            bla.Left = 400;
+            
+            // bla.Top = n.wagen.Margin.Top;
+            x.To = bla;
+            x.Duration = TimeSpan.Parse("0:0:3");
+            x.BeginTime = TimeSpan.Parse("0:0:" + Kunde.kundennummer);
+            for (int zaehl = 0; zaehl < anzKunden; zaehl++)
+            {
+                Kunde n = new Kunde();
+                n.wagen.MouseDown += Wagen_MouseDown;
+                n.wagen.Margin = new Thickness(20 * Kunde.kundennummer, 30 * Kunde.kundennummer, 0, 0);
+                raumdarstellung.Children.Add(n.wagen);
+
+                bla.Top = n.wagen.Margin.Top;
+                x.AutoReverse = autoreverse;
+                n.wagen.BeginAnimation(Image.MarginProperty, x);
+                autoreverse = !autoreverse;
+
+            }
+
+        }
+        private void Wagen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
         }
     }
 }
