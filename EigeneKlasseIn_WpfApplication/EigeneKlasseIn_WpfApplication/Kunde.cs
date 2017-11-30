899b5765a670992d23bf06754b547150355c9b74
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2017-11-30 11:23:35 +0100

    Klasse Darstellung auf Canvas

---
+++ EigeneKlasseIn_WpfApplication/EigeneKlasseIn_WpfApplication/Kunde.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace EigeneKlasseIn_WpfApplication
+{
+    public class Kunde
+    {
+        public static int kundennummer;
+        public Image wagen = new Image();      // WPFKlasse in normaler .cs Umgebung verwenden
+                                        // entsprechendes using muss verwendet werden, in dem Fall using System.Windows.Controls;
+
+        public Kunde()
+        {
+            kundennummer++;
+            wagen.Source = 
+                new BitmapImage(new Uri("C:\\Users\\kaffeetier\\Documents\\c#\\WPF\\EigeneKlasseIn_WpfApplication\\EigeneKlasseIn_WpfApplication\\shopping-cart.png"));
+        }
+    }
+}
---
+++ EigeneKlasseIn_WpfApplication/EigeneKlasseIn_WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EigeneKlasseIn_WpfApplication
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Kunde.kundennummer = 1;
+
+            // kann man Kunde einfügen, aber dann ist kein fertiges Fenster gezeichnet
+            // Kunde heinz = new Kunde();
+            
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Kunde heinz = new Kunde();
+            raumdarstellung.Children.Add(new Image() { Source = heinz.wagen.Source });
+            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count-1], 30);
+            Kunde meier = new Kunde();
+            raumdarstellung.Children.Add(new Image() { Source = meier.wagen.Source });
+            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 60);
+            Kunde schulz = new Kunde();
+            raumdarstellung.Children.Add(new Image() { Source = schulz.wagen.Source });
+            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 90);
+            Kunde schmidt = new Kunde();
+            raumdarstellung.Children.Add(new Image() { Source = schmidt.wagen.Source });
+            Canvas.SetLeft(raumdarstellung.Children[raumdarstellung.Children.Count - 1], 120);
+        }
+    }
+}
