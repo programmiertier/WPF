77e8ab58f6889bbe122a3e74b9d3f1c0dd4d84c0
programmiertier <zimmermannveronique@protonmail.com>
2017-12-04 08:54:38 +0100

    Wunschliste mit Drag und Drop

---
+++ Wunschliste_WpfApplication/Wunschliste_WpfApplication/Kram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wunschliste_WpfApplication
+{
+    class Kram
+    {
+        private string _bezeichnung;
+        private double _preis;
+
+        public string bezeichnung { get { return _bezeichnung; } set { _bezeichnung = value; } }
+        public double preis { get { return _preis; } set { _preis = value; } }
+
+    }
+}
